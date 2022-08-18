b27ac880a53e5f5d3dbbb4a1eb95ebe1ece4dc3b
femow <felipemu_92@hotmail.com>
2022-08-17 23:03:39 -0300

    basic

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const { v4: uuidv4 } = require("uuid")
+
+const app = express()
+app.use(express.json())
+
+const customers = []
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body
+    const customerAlreadyExists =
+        customers.some(customer =>
+            customer.cpf === cpf)
+    
+    if (customerAlreadyExists) {
+        return res.status(400).json({ error: "Customer already exists!" })
+    }
+
+    customers.push({
+        id: uuidv4(),
+        cpf,
+        name,
+        statement: []
+    })
+
+    return res.status(201).send();
+})
+
+app.get("/statement", (req, res) => {
+    const { cpf } = req.headers;
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return res.status(400).json({ error: "Customer not found" });
+    }
+
+    return res.json(customer.statement);
+})
+
+app.listen(3333)
